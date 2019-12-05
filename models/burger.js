90daaf40b1a3ac27c67fa7c52c8c48b2f211282d
Tyler Shuck <tshuck88@gmail.com>
2019-12-05 14:28:37 -0800

    created routes in the controller

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-// Import the ORM to create functions that will interact with the database.
+
 var orm = require("../config/orm.js");
 
 var burger = {
@@ -7,7 +7,7 @@ var burger = {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+
   insertOne: function(cols, vals, cb) {
     orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
@@ -20,5 +20,4 @@ var burger = {
   }
 };
 
-// Export the database functions for the controller (catsController.js).
 module.exports = burger;
