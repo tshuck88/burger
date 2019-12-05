19065c441258a67d10aed6edefee0615a5e23305
Tyler Shuck <tshuck88@gmail.com>
2019-12-05 13:41:46 -0800

    created server and orm

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   database: "burger_db"
 });
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +17,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
