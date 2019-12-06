d87352675cc9cf9941679326a6b2f9e159903a72
Tyler Shuck <tshuck88@gmail.com>
2019-12-05 22:57:08 -0800

    fixed typo

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -19,7 +19,7 @@ router.post("/api/burgers", function(req, res) {
   burger.insertOne([
     "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function(result) {
 
     res.json({ id: result.insertId });
