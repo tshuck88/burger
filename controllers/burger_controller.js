a2d7d152d8027868ff4c84978ecd6bc5401115a4
Tyler Shuck <tshuck88@gmail.com>
2019-12-05 22:53:30 -0800

    fixed a typo and moved data-id around

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.insertOne([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
